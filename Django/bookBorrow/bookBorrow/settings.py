7f6b427e32a914f06464e1ce44572687477c8bfe
cjs9319786 <chh978670@gmail.com>
2025-12-07 10:41:57 +0900

    DB 프로젝트 결과파일

--- Django/bookBorrow/bookBorrow/settings.py
+++ Django/bookBorrow/bookBorrow/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'librarydb',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
